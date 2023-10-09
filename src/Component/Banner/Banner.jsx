fb5b9a17d49f052a58b6c9185202baf44cfc66cf
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-10-09 23:53:38 +0600

    drop down color fix

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -2,9 +2,9 @@
 
 const Banner = () => {
     return (
-        <div className='h-[80vh] flex justify-center items-center'>
+        <div className='h-[80vh] flex justify-center text-center items-center'>
         
-                <p data-aos="fade-up" className="text-6xl bold uppercase text-white outline p-5">We Love To Create Memories</p>
+                <p data-aos="fade-up" className=" text-3xl md:text-6xl bold uppercase text-white outline p-5">We Love To Create Memories</p>
             
         </div>
     );
